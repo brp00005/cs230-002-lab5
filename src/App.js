8108071d1cb22541943f35a71e7f46a7473b391a
brp00005 <brp00005@mix.wvu.edu>
2023-10-31 15:53:56 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Card from './Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CS 230L</h1>
+      <h2>Section - 02</h2>
+      <p>WVU ID: 800367108</p>
+      <p>Hi, I am Branden Purdum</p>
+
+      <Navbar />
+      <Card />
+
+
+      
     </div>
+
+
   );
 }
 
